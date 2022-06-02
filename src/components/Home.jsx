be883803cccc51e840f46d35daaf204a226c526d
muafa7 <muafammm@gmail.com>
2022-06-02 17:13:09 +0700

    adding form input function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import '../App.css';
 
 const Home = () => {
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('')
+    
+    //function when input got submit
+    const emailSubmit = event => {
+        event.preventDefault();
+        if (validateEmail()) {
+            setMessage(`We have sent email to ${email}`);
+        } else {
+            setMessage("Please input valid email address");
+        }
+    }
+    //validate email format
+    const validateEmail = () => {
+        let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (!filter.test(email)) {
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+
     return (
         <div className="home">
+            {/* jumbotron section */}
             <section class="jumbotron">
                 <div class="content">
                     <div class="block-content">
@@ -15,15 +40,20 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+            {/* contact section */}
             <section class="contact">
                 <div class="contact-content">
                     <h1>STAY IN THE KNOW</h1>
                     <h2>Enter your email address to recieve news and updates from Jones Bar-B-Q.</h2>
                 </div>
-                <form id="contact-input" onsubmit="emailSubmit();return false" action="#">
-                    <input class="email-input" type="text" name="email" id="email" placeholder="Email Address"/>
+                <form onSubmit={emailSubmit} >
+                    <input class="email-input" type="text" value={email} placeholder="Email Address" onChange={(e)=> setEmail(e.target.value)} />
                     <input class="email-submit" type="submit" value="SUBMIT"/>
                 </form>
+                {/* message display when form got submit */}
+                {message && (
+                    <p>{message}</p>
+                )}
             </section>
         </div>
     )
